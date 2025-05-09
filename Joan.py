feb22a02842487615320a1ecb712eeabe2a7c915
Haibo2005 <sm17.xia_haibo@iessantamariadeivissa.com>
2025-05-09 19:51:33 +0200

    Update Joan.py

--- Joan.py
+++ Joan.py
@@ -1,1 +1,13 @@
+mamifero = {'perra' , 'gorila' , 'mono' ,'macaco' }
+aves = {'cuervo' , 'pato' , 'gallina', 'agila'}
+reptil = {'serpiente' , 'lagartija', 'comodo' , 'iguana'}
 
+animal = str(input('Introduzca un animal : ')).upper()
+if animal in mamifero : 
+    print(f'El animal es un mono ')
+elif animal in aves : 
+    print(f'El animal es un ave ')
+elif animal in reptil : 
+    print(f'El animal es un reptil ')
+else : 
+    print(f'Es otro tipo de animal')
